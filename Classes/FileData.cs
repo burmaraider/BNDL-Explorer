d9a60c56cb818205e2920373f4a90c60ca52080b
thecanonmaster <thecanonmaster@gmail.com>
2021-02-14 22:46:13 +0300

    Commit after rework

---
+++ Classes/FileData.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace BNDL_Explorer.Classes
+{
+    public class FileData
+    {
+        public FileData(bool skipped)
+        {
+            this.skipped = skipped;
+            offset = 0;
+            chunkSize = 0;
+            stringOffset = 0;
+        }
+
+        public void SetName(string name)
+        {
+            fullName = name;
+            nameAndDirs = name.Split('\\');
+            nameOnly = Path.GetFileName(name);
+            extension = Path.GetExtension(name);
+        }
+
+        public bool skipped;
+        public string nameOnly;
+        public string fullName;
+        public string[] nameAndDirs;
+        public string extension;
+        public long offset;
+        public int stringOffset;
+        public uint chunkSize;
+        public uint chunkSizeReported;
+    }
+}
